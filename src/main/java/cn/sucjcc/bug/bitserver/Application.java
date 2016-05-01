c04e28c016ae25555d265fd1fc7da2234a327b48
lilujia <dujiajucode@163.com>
2016-05-02 03:20:28 +0800

    init

---
+++ src/main/java/cn/sucjcc/bug/bitserver/Application.java
@@ -0,0 +1,16 @@
+package cn.sucjcc.bug.bitserver;
+
+/**
+ * Created by lilujia on 16/5/2.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/cn/sucjcc/bug/bitserver/Greeting.java
@@ -0,0 +1,23 @@
+package cn.sucjcc.bug.bitserver;
+
+/**
+ * Created by lilujia on 16/5/2.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
